a1dcc50af19821609197d88b8c6bddb80aaa4e5b
Adekola Akano <akano.adekola@gmail.com>
2024-08-09 19:02:32 +0100

    Released first version

---
+++ src/main/kotlin/com/adekola/curlToRetrofit/curl/TestJava.java
@@ -0,0 +1,10 @@
+package com.adekola.curlToRetrofit.curl;
+
+
+public class TestJava {
+//    @SuppressWarnings("UnstableApiUsage")
+//    @ApiStatus.Experimental
+//    public interface BuildProgress<T extends BuildProgressDescriptor> {
+//
+//    }
+}
